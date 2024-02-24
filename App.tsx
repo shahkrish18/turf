4b21344876b018502df7891b46c1e5a1a5cbbd58
shahkrish18 <shahkrish181003@gmail.com>
2024-02-24 17:24:18 +0530

    login page ready

--- App.tsx
+++ App.tsx
@@ -1,118 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Login from './screens/Login';
+import Signin from './screens/Signin';
+import Welcome from './screens/Welcome';
+import Popup from './screens/Popup';
+import Home from './screens/Home';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const Stack = createNativeStackNavigator();
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Welcome'>
+        <Stack.Screen name="Welcome" options={{headerShown:false}} component={Welcome} />
+        <Stack.Screen name="Login" options={{headerShown:false}} component={Login} />
+        <Stack.Screen name="Signin" options={{headerShown:false}} component={Signin} />
+        <Stack.Screen name="Popup" options={{headerShown:false}} component={Popup} />
+        <Stack.Screen name="Home" options={{headerShown:false}} component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
---
+++ screens/Popup.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+import { View, Text } from 'react-native';
+import tw from 'twrnc'
+
+const SplashScreen = ({navigation}:any) => {
+  const navigationRef = useRef(navigation);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // Navigate to another screen after 3 seconds
+      navigationRef.current.replace('Home');
+    }, 3000); // 3000 milliseconds = 3 seconds
+
+    return () => clearTimeout(timeout); // Clear the timeout if component unmounts
+  }, []);
+
+  return (
+    <View style={{ flex: 1,margin:35, alignItems: 'center', padding:5 }}>
+      <View style={tw`bg-gray-200 p-2`} >
+        <Text style={tw`text-black`} >Logo</Text>
+      </View>
+      <View style={tw`mt-30`}>
+      <Text style={tw`text-black text-6xl`} >All your favourite turfs in one app</Text>
+      </View>
+    </View>
+  );
+};
+
+export default SplashScreen;
